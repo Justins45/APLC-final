388dd5febb1f308db85e46eba41f5d0ac5e84920
Jason Grant <141342785+jasongrant902@users.noreply.github.com>
2025-12-17 10:18:19 -0700

    First class pushed

---
+++ src/Vehicle.java
@@ -0,0 +1,30 @@
+public class Vehicle {
+
+    private String plate;
+    private String size;
+
+
+    //getters
+    public String getPlate() {
+        return vehicle;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    //setters
+    public void setPlate(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    //constructor
+    public Vehicle(String plate, String size){
+        this.plate = _vehicle;
+        this.size = _size;
+    }
+}
